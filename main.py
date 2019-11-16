7a74f962717af1e13b9353481965c513221487c7
Murillo-Acacio <murilloaccacio@gmail.com>
2019-11-16 19:24:54 -0400

    loading

--- main.py
+++ main.py
@@ -7,14 +7,19 @@ screen = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Projeto Final')
 
 background = pygame.image.load("sprites/background-menu(800:450).jpg").convert_alpha()
+knight = pygame.image.load("sprites/Knight/knight.png").convert_alpha()
 
 def redraw_background():
     screen.blit(background, (0, 0))
+
+def redraw_knight(x, y):
+    screen.blit(knight, (x, y))
 
 run = init = True
 
 while run:
     redraw_background()
+    redraw_knight(0, 300)
 
     # aqui ele sai do loop quando aperta no "X"
     for event in pygame.event.get():
