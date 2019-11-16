e032e6951dc8c0fcad65ba098344af3aba4e1ae8
Murillo-Acacio <murilloaccacio@gmail.com>
2019-11-16 12:04:32 -0400

    atualizando

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import pygame
-import sprites
-
-
 pygame.init()
+
 largura = 800
 altura = 450
 screen = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Projeto Final')
-pygame.display.flip()
 
 background = pygame.image.load("sprites/background-menu(800:450).jpg").convert_alpha()
 
@@ -18,11 +15,12 @@ run = init = True
 
 while run:
     redraw_background()
-    pygame.display.update()
 
     # aqui ele sai do loop quando aperta no "X"
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    pygame.display.flip()
 
 pygame.quit()
--- menu.py
+++
@@ -1,5 +0,0 @@
-import pygame
-import sprites
-def initial(screen):
-	pygame.image.load("sprites/background-menu.jpg")
-initial(1);
